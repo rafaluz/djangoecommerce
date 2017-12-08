9fcd43be0f4dd02653602dc5b23c9fe467272637
root <root@rafa.localdomain>
2017-12-08 03:21:16 +0000

    antes da ultima aula modulo 2

--- core/views.py
+++ core/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Category
+
 def index(request):
 	#return HttpResponse('Hello World')
 	#textos = ['rafael 1', 'rafael 2', 'rafael 3']
 	#context = {
 	#	'title': 'django e-commerce',
-	#	'textos' : textos
-	#} 
+	#	'textos' : textos,
+	#	'categories': Category.objects.all(),
+	#}
 	return render(request, 'index.html')
 
 
 def contact(request):
 	return render(request, 'contact.html')
 
-def product_list(request):
-	return render(request, 'product_list.html')
-
-def product(request):
-	return render(request, 'product.html')	
