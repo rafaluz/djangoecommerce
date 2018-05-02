fbca721f17dc2c654b8694b49c4780ea4e8895e8
root <root@rafa.localdomain>
2018-05-02 00:59:25 +0000

    aula 39

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -145,6 +145,10 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'admin@djangoecommerce.com'
 
+#Auth
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
+
 try:
     from .local_settings import *
 except ImportError:
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -19,9 +19,13 @@ from django.contrib import admin
 from core import views
 from catalog import views as views_catalog
 
+from django.contrib.auth.views import login, logout
+
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^contato/', views.contact, name='contact'),
+    url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'index'}, name='logout'),
 	url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
